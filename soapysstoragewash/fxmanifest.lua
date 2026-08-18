31056e4952153ddcbe8dc274a1883f2cc90ebb60
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2026-08-18 13:59:26 -0400

    Add files via upload

---
+++ soapysstoragewash/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+version '1.0.0'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/harmony_carwash_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/harmony_ground_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/harmony_sheds_lod.ytyp'
